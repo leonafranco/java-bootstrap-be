f4f1a20380d90cb4f989fbcac8fa4bb1e743f85e
leonafranco <leonardomf96@gmail.com>
2024-01-28 00:02:11 +0000

    first commit

---
+++ src/main/java/com/noWhat/noWhat/Entities/User.java
@@ -0,0 +1,72 @@
+package com.noWhat.noWhat.Entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+}
+
+
---
+++ src/main/java/com/noWhat/noWhat/NoWhatApplication.java
@@ -0,0 +1,13 @@
+package com.noWhat.noWhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoWhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoWhatApplication.class, args);
+	}
+
+}
