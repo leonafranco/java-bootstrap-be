e03477217c9324781a6173cbe7bcc8e0a99e3d12
leonafranco <leonardomf96@gmail.com>
2024-02-25 15:17:38 +0000

    bootstrap app

---
+++ src/test/java/com/noWhat/noWhat/MyUnitTest.java
@@ -0,0 +1,12 @@
+package com.noWhat.noWhat;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+    @Test
+    public void testSomething() {
+        // Test your code here
+        assertTrue(true);
+    }
+}
